45d3de829f297bb20d63d418e51d3e9023fd0fd0
Riley1101 <arkardev00@gmail.com>
2026-03-22 12:01:35 -0400

    Add make file
    Add terminal scroll
    
    Fix green terminal

--- first-kernel/kernel.c
+++ first-kernel/kernel.c
@@ -68,7 +68,7 @@ uint16_t *terminal_buffer = (uint16_t *)VGA_MEMORY;
 void terminal_initialize(void) {
   terminal_row = 0;
   terminal_column = 0;
-  terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+  terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);
 
   for (size_t y = 0; y < VGA_HEIGHT; y++) {
     for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -78,26 +78,43 @@ void terminal_initialize(void) {
   }
 }
 
+void terminal_scroll() {
+  for (size_t y = 0; y < VGA_HEIGHT - 1; y++) {
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+      terminal_buffer[y * VGA_WIDTH + x] =
+          terminal_buffer[(y + 1) * VGA_WIDTH + x];
+    }
+  }
+  for (size_t x = 0; x < VGA_WIDTH; x++) {
+    terminal_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH + x] =
+        vga_entry(' ', terminal_color);
+  }
+}
+
 void terminal_setcolor(uint8_t color) { terminal_color = color; }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
   const size_t index = y * VGA_WIDTH + x;
-  if (c == '\n') {
-    terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT) {
-      terminal_row = 0;
-    }
-    return;
-  }
   terminal_buffer[index] = vga_entry(c, color);
 }
 
 void terminal_putchar(char c) {
+  if (c == '\n') {
+    terminal_column = 0;
+    if (++terminal_row == VGA_HEIGHT) {
+      terminal_scroll();
+      terminal_row--;
+    }
+    return;
+  }
+
   terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+
   if (++terminal_column == VGA_WIDTH) {
     terminal_column = 0;
     if (++terminal_row == VGA_HEIGHT) {
-      terminal_row = 0;
+      terminal_scroll();
+      terminal_row--;
     }
   }
 }
@@ -114,5 +131,9 @@ void terminal_writestring(const char *data) {
 
 void kernel_main(void) {
   terminal_initialize();
-  terminal_writestring("HELLO, kernel world!\n");
+  for (char c = 'A'; c <= 'Z'; c++) {
+    char *ca = &c;
+    terminal_writestring(ca);
+    terminal_writestring("\n");
+  }
 }
